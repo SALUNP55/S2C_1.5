0a197e7edb6c916ec807b84cf2ea1b4b0fdee857
Prateek Salunke <prateek.salunke.experisit@whirlpool.com>
2018-09-25 18:01:49 +0530

    Api changes

--- S2C_1.5/src/test/java/Com/S2C/BaseClass/TestClass.java
+++ S2C_1.5/src/test/java/Com/S2C/BaseClass/TestClass.java
@@ -1,69 +1,93 @@
 package Com.S2C.BaseClass;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
 
+import org.json.JSONObject;
 
+import com.google.gson.JsonObject;
 
+import okhttp3.FormBody;
 import okhttp3.Headers;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 
 
 public class TestClass {
-
+	String json;
 	// TODO Auto-generated method stub
 
 	public static final MediaType JSON = MediaType.get("application/x-www-form-urlencoded");
 	OkHttpClient client = new OkHttpClient();
 
-	String post(String url, String json) throws IOException {
+	String post() throws IOException {
 		
-		RequestBody body = RequestBody.create(JSON, json);
-		System.out.println(body.contentType());
-		Headers header = Headers.of("Content-Type", "application/x-www-form-urlencoded", "client_id", "syntel","client_secret", "syntel123$");
-		System.out.println(header);
+		//RequestBody body = RequestBody.create(JSON, json);
+		//System.out.println(body.contentType());
+		//Headers header = Headers.of("Content-Type", "application/x-www-form-urlencoded", "client_id", "syntel","client_secret", "syntel123$");
+		//System.out.println(header);
+		RequestBody body = new FormBody.Builder()
+				.add("grant_type", "password")
+				.add("client_id","syntel")
+				.add("client_secret", "syntel123$")
+				.add("username", "minerva5@mailforspam.com")
+				.add("password", "Smart2000")
+				.build();
 		Request request = new Request.Builder()
-				.url(url)
-				.headers(header)
+				.url("https://api-stg.whrcloud.com/oauth/token")
 				.post(body)
 				.build();
 		try (Response response = client.newCall(request).execute()) {
-			return response.body().string();
+		
+			//ResponseBody Response =response.body().create(JSON, response.toString())
+	
+			JSONObject jobject = new JSONObject(response.body().string());
+			return jobject.get("access_token").toString();
+		
 		}
 	}
-
-	/*String bowlingJson() {
-		String password = "password";
-		String email = "tedmarketplaces@gmail.com";
-		String userPassword = "Smart2000";
-		String json ="\"grant_type\":\"" + password.trim() + "\",\n" + "\"client_id\":\"syntel\",\n" +"\"client_secret\":\"syntel123$\",\n"
-				+ "\"username\":\"" + email + "\",\n" + "\"password\":\"" + userPassword + "\"\n" ;
-		return json;
-
-	}*/
-	String bowlingJson() {
-		return "\n{\ngrant_type: 'password',"
-				+ "\nclient_id:'syntel',"
-						+ "\nclient_secret:'syntel123$',"
-						+ "\nusername:'tedmarketplaces@gmail.com',"
-						+ "\npassword:'Smart2000'\n}";
-						
+	
+String post2() throws IOException {
+		
+		//RequestBody body = RequestBody.create(JSON, json);
+		//System.out.println(body.contentType());
+		//Headers header = Headers.of("Content-Type", "application/x-www-form-urlencoded", "client_id", "syntel","client_secret", "syntel123$");
+		//System.out.println(header);
+		
+		Request request = new Request.Builder()
+				.url("https://api-stg.whrcloud.com/api/v1/masterApplianceCategories")
+				.header("Authorization", ("Bearer"+post()))
+				.addHeader("wp-client-region", "NAR")
+				.addHeader("wp-client-country", "US")
+				.addHeader("wp-client-brand", "WHIRLPOOL")
+				.addHeader("wp-client-language", "ENG")
+		        .build();
+		try (Response response = client.newCall(request).execute()) {
+		
+			//ResponseBody Response =response.body().create(JSON, response.toString())
+	
+			//JSONObject jobject = new JSONObject(response.body().string());
+			//return jobject.get("access_token").toString();
+			return response.body().string();
+		
+		}
 	}
 	
 	
-
-          
-	
+				
 	  
 	public static void main(String[] args) throws IOException {
 		TestClass example = new TestClass();
-		String json = example.bowlingJson();
-		String response = example.post("https://api.us.apiconnect.ibmcloud.com/whirlpool-developer-network-wcloud-dev/whirlpool-iot-qa/oauth/token",json);
-		System.out.println(response);
+	    String access_token = example.post();
+	    System.out.println(access_token);
+	   System.out.println(example.post2()); 
+        
 
 	}
 }
